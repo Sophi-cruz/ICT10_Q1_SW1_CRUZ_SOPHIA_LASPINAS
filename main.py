a63d0b73525c07672d99fd651c0026ea281bdee5
Sophi-cruz <cruz.sb@obmontessori.edu.ph>
2026-09-04 11:51:46 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+#Cruz, De Guzman
+studname = "Ranee Anja  De Guzman" #string
+studage = 14 #interger
+studheight = 151.8 #float
+countries_vists = [" Germany, Japan, Russia "] #list
+student_type = False #boolean
+stud_pref = {
+    "color": "red",
+    "car brand" : "Jeep",
+    "shoes size" : 10,
+    "best friend" : "Cheesewheel"
+    }
+stud_fruits = {
+    "JackFruit",
+    "Raspberries",
+    "Blueberry",
+    "Mango"
+    }
+days_week = (
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+    )
+
+print("Name: , studname")
+print("Age: , studage")
+print("Height : , studheight")
+print("Countries : , countries_vists")
+print("Preferences : , studpref")
+print("Fruits : , studfruits")
+print("Days_of_the_week : , days_weeks")
